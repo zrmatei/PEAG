c9290a5a3d0c123109544ee69014496cb50302b6
zrmatei <149681533+zrmatei@users.noreply.github.com>
2024-05-10 18:43:58 +0300

    adaugat turneu
    Am adaugat si selectia prin turneu

--- testing1/select.py
+++ testing1/select.py
@@ -49,3 +49,21 @@ def SelectieParinti():
     pop_initiala = numpy.asarray(pop_initiala)
     parinti = numpy.asarray(parinti)
     return pop_initiala, parinti
+
+
+def turneu(pop_initiala, dim, n, k):
+    parinti = pop_initiala.copy()
+    fitnessuri = numpy.zeros(dim)
+    for i in range(dim):
+        fitnessuri[i] = pop_initiala[i][n]
+    for i in range(dim):
+        submultimePozitiiIndivizi = numpy.random.randint(0, dim, k)
+        fitnessuriIndiviziSelectati = [fitnessuri[submultimePozitiiIndivizi[i]] for i in range(k)]
+        fitnessIndividCastigator = max(fitnessuriIndiviziSelectati)
+        pozMax = numpy.where(fitnessuriIndiviziSelectati == fitnessIndividCastigator)
+        indexPozMax = pozMax[0][0]
+        # index = corespondentul lui indexPozMax in pop_initiala
+        index = submultimePozitiiIndivizi[indexPozMax]
+        parinti[i][0:n] = pop_initiala[index][0:n]
+        parinti[i][n] = fitnessuri[index]
+    return parinti
